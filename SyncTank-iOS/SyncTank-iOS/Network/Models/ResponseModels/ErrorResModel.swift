318bdfc9dbaa7128e4b924118f856b02b433a477
keon22han <keonheehan@hansung.ac.kr>
2025-09-06 23:11:05 +0900

    feat: ResModel 구현
    ResModel 구현

---
+++ SyncTank-iOS/SyncTank-iOS/Network/Models/ResponseModels/ErrorResModel.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorResModel.swift
+//  SyncTank-iOS
+//
+//  Created by keonheehan on 9/6/25.
+//
+
+struct ErrorResModel: Decodable { }
---
+++ SyncTank-iOS/SyncTank-iOS/Network/Models/ResponseModels/SaveDocsResModel.swift
@@ -0,0 +1,10 @@
+//
+//  SaveDocsResModel.swift
+//  SyncTank-iOS
+//
+//  Created by keonheehan on 9/6/25.
+//
+
+struct SaveDocsResModel: Decodable {
+    let message: String
+}
